effaa193efd79bf47ce16080026fd1414d5248b8
Thos <thomasle993@gmail.com>
2020-08-02 14:32:02 -0500

    Added Send Email messge function to the home controllers. Also Added Index.js.

--- DesignTransformations.UI.MVC/Controllers/HomeController.cs
+++ DesignTransformations.UI.MVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using System.Net.Mail;
 
 namespace DesignTransformations.UI.MVC.Controllers
 {
@@ -25,6 +26,37 @@ namespace DesignTransformations.UI.MVC.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult SendEmail(string contact_email, string contact_name, string subject, string body)
+        {
+            string data = "0";
+            try
+            {
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = System.Configuration.ConfigurationManager.AppSettings["Host"].ToString();
+                smtp.Port = System.Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["port_ssl"].ToString());
+                smtp.EnableSsl = System.Convert.ToBoolean(System.Configuration.ConfigurationManager.AppSettings["tls_ssl_req"].ToString());
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential(System.Configuration.ConfigurationManager.AppSettings["emailAddress"].ToString(),
+                    System.Configuration.ConfigurationManager.AppSettings["pwd"].ToString());
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                MailMessage msg = new MailMessage(contact_email, System.Configuration.ConfigurationManager.AppSettings["emailAddress"].ToString());
+                msg.Subject = subject;
+                msg.Body = body;
+                msg.IsBodyHtml = false;
+                msg.Attachments.Clear(); //this is to help with security by hopefully removing any attachments present - tle 2020-07-28
+                smtp.Send(msg);
+                data = "success";
+            }
+            catch (System.Exception ex)
+            {
+
+                data = ex.Message + " " + ex.StackTrace;
+            }
+
+            return new JsonResult() { Data = "data" };
         }
     }
 }
---
+++ DesignTransformations.UI.MVC/Scripts/Index.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+    /*
+     * pasted for refernce purposes - tle 2020-08-02
+                                   <div class="loading"> Loading</div>
+                                <div class="error-message"> </div>
+                                <div class="sent-message"> Your message has been sent. Thank you!</div>
+     * */
+    $('.loading').hide();
+    $('.error-message').hide();
+    $('.sent-message').hide();
+
+    $("#sendMessage").click(function () {
+
+        $('.loading').show();
+
+        var contact_name = $('#name').val();
+        var contact_email = $('#email').val();
+        var subject = $('#subject').val();
+        var body = $('#message').val();
+        $.ajax({
+            type: 'POST',
+            url: "/Home/SendEmail/",
+            //(string contact_email,string contact_name,string subject,string body)
+            data: {'contact_email': contact_email, 'contact_name':contact_name, 'subject':subject, 'body': body},
+            success: function (d,data) {
+                $('.sent-message').show();
+                $('.loading').hide();
+            },
+            error: function (response, error) {
+                $('.error-message').append(error);
+                $('.error-message').show();
+                $('.loading').hide();
+            }
+        });
+    });
+
+});
